220467b5422b7d8045517d99c3d929ae5e400f5f
cnsaez <csaez@rsolver.com>
2014-09-06 07:27:46 -0400

    Add new POME and new POC as test models
    POME is sent by the GM to the SCM then to LongTen and back to SCM
    POC is created once SCM recives the Product from China
    (If something is weird let me know)

---
+++ Models/NewPOC.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCMS.Models
+{
+    public class NewPOC
+    {
+
+        public int NewPOCID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public virtual DestinationPort DestinationPort { get; set; }
+
+        //Get from Shipments
+        public int AmountTotal { get; set; }
+
+        //From Contract
+        public double PricePerTon { get; set; }
+
+        //From Contract
+        public virtual Product Product { get; set; }
+
+        //From Contract
+        public virtual Incoterm CustomerIncoterm { get; set; }
+
+        public int Status { get; set; }
+
+        public string Comments { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? CreationDate { get; set; }
+
+        public virtual ICollection<Shipment> Shipments { get; set; }
+
+        public int ShipmentCount
+        {
+            get
+            {
+                int count = 0;
+                if (Shipments != null)
+                {
+                    foreach (Shipment Shipment in Shipments)
+                    {
+                        //For when shipment have status
+                        //if (Shipment.Status != -1)
+                            count++;
+                    }
+                }
+                return count;
+            }
+        }
+
+        public string StatusString { get 
+            { 
+                switch(Status){
+                    case -1:
+                        return "Cancelled";
+                    case 1:
+                        return "Incomplete";
+                    case 2:
+                        return "Sent";
+                    case 3:
+                        return "Dividing";//Is being divided into POMEs
+                    case 4:
+                        return "Scheduled";//Has been divided into POMEs
+                    default:
+                        return "Unknown";
+                }
+            } 
+        }
+
+        public int TonsLeft { 
+            get 
+            {
+                int left = AmountTotal;
+                if (Shipments != null)
+                {
+                    foreach (Shipment Shipment in Shipments)
+                    {
+                        //if (Shipment.Status != -1)
+                            left -= Shipment.Amount;
+                    }
+                }
+                return left;
+            } 
+        }
+    }
+}
---
+++ Models/NewPOME.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SCMS.Models
+{
+    public class NewPOME
+    {
+        public int NewPOMEID { get; set; }
+
+        /*LongTen Client:
+         * Elecmetal Chile
+         * USA
+         * Hong Kong         
+         */
+        public virtual Customer Customer { get; set; }
+
+        //Only associated with the customers
+        public virtual DestinationPort DestinationPort { get; set; }
+
+        public int AmountTotal { get; set; }
+         
+        public double PricePerTon { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        /*Only:
+         * FOB
+         * FCA
+         * EXW
+         */
+        public virtual Incoterm CustomerIncoterm { get; set; }
+
+        public int Status { get; set; }
+
+        public string SpecialRequirements { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? CreationDate { get; set; }
+
+        public string StatusString
+        {
+            get
+            {
+                switch (Status)
+                {
+                    case -1:
+                        return "Cancelled";
+                    case 1:
+                        return "Incomplete";
+                    case 2:
+                        return "Sent";
+                    case 3:
+                        return "Dividing";
+                    case 4:
+                        return "Scheduled";
+                    default:
+                        return "Unknown";
+                }
+            }
+        }
+    }
+}
